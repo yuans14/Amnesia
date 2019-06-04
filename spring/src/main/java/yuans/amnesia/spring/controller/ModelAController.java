68f6e3a40e61a754614186b7d510f42954fc6d82
yuans14 <yuans14@outlook.com>
2019-06-04 18:09:40 +0800

    use simpleJpaRepository

--- spring/src/main/java/yuans/amnesia/spring/controller/ModelAController.java
+++ spring/src/main/java/yuans/amnesia/spring/controller/ModelAController.java
@@ -2,7 +2,6 @@ package yuans.amnesia.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import yuans.amnesia.spring.entity.ModelA;
 import yuans.amnesia.spring.repository.ModelARepository;
@@ -12,15 +11,15 @@ import java.util.List;
 @RestController
 public class ModelAController {
 
-    private final ModelARepository modelARepository;
+    private final ModelARepository repository;
 
     @Autowired
-    public ModelAController(ModelARepository modelARepository) {
-        this.modelARepository = modelARepository;
+    public ModelAController(ModelARepository repository) {
+        this.repository = repository;
     }
 
-    @GetMapping("/{code}")
-    public List<ModelA> findModelA(@PathVariable("code") String code){
-        return modelARepository.findByCode(code);
+    @GetMapping("/all")
+    public List<ModelA> findAll(){
+        return repository.findAll();
     }
 }
--- spring/src/main/java/yuans/amnesia/spring/repository/ModelARepository.java
+++ spring/src/main/java/yuans/amnesia/spring/repository/ModelARepository.java
@@ -1,11 +1,8 @@
 package yuans.amnesia.spring.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import yuans.amnesia.spring.entity.ModelA;
 
-import java.util.List;
+public interface ModelARepository extends JpaRepository<ModelA, Long> {
 
-public interface ModelARepository extends CrudRepository<ModelA, Long> {
-
-    List<ModelA> findByCode(String code);
 }
