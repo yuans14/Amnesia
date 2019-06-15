c2f50b3c286e5b118b09d45d5507659308d5fb5e
yuans14 <yuans14@outlook.com>
2019-06-15 16:13:35 +0800

    add cache

---
+++ spring/src/main/java/yuans/amnesia/spring/CacheConfiguration.java
@@ -0,0 +1,9 @@
+package yuans.amnesia.spring;
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableCaching
+public class CacheConfiguration {
+}
--- spring/src/main/java/yuans/amnesia/spring/controller/ModelController.java
+++ spring/src/main/java/yuans/amnesia/spring/controller/ModelController.java
@@ -1,9 +1,13 @@
 package yuans.amnesia.spring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.util.Assert;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -16,8 +20,6 @@ import org.springframework.web.bind.annotation.RestController;
 import yuans.amnesia.spring.entity.Model;
 import yuans.amnesia.spring.repository.ModelRepository;
 
-import java.util.Date;
-
 @RestController
 public class ModelController {
 
@@ -28,35 +30,43 @@ public class ModelController {
         this.repository = repository;
     }
 
+    @GetMapping("/v1/models/{id}")
+    @Cacheable("model")
+    public Model findById(@PathVariable("id") Long id) {
+        return repository.findById(id).orElse(null);
+    }
     @GetMapping("/v1/models")
+//    @Cacheable("models")
     public Page<Model> find(@PageableDefault(sort = {"id"}) Pageable pageable) {
         return repository.findAll(pageable);
     }
 
     @PostMapping("/v1/models")
+    @CachePut(value = "model", key = "#result.id")
     public Model save(@RequestBody Model model) {
-        model.setId(null);
+        Assert.isNull(model.getId(), "The given id must be null");
         return repository.save(model);
     }
 
-    @PutMapping("/v1/models/{id}")
-    public Model update(@PathVariable("id") Long id,
-                        @RequestBody Model model) {
-        model.setId(id);
+    @PutMapping("/v1/models")
+    @CachePut(value = "model", key = "#result.id")
+    public Model update(@RequestBody Model model) {
+        Assert.notNull(model.getId(), "The given id must not be null");
         return repository.save(model);
     }
 
     @PatchMapping("/v1/models/{id}")
+    @CachePut(value = "model", key = "#result.id")
     public Model update(@PathVariable("id") Long id,
                         @RequestParam("code") String code) {
         Model model = repository.findById(id)
                 .orElseThrow(() -> new IllegalArgumentException("The model must exist"));
         model.setCode(code);
-        model.setOperateTime(new Date());
         return repository.save(model);
     }
 
     @DeleteMapping("/v1/models/{id}")
+    @CacheEvict("model")
     public void delete(@PathVariable("id") Long id) {
         repository.deleteById(id);
     }
